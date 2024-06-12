94a954c8de1d9fa58b25da4c3274e0cb0e1a2f2c
Владислав Дворянинов <v.dvoryaninov@notamedia.ru>
2024-06-12 14:46:59 +0300

    Разработка слайдера

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,10 @@
 /* в этот файл добавляет скрипты*/
+window.addEventListener('load' , () => {
+  /*const testEl = document.querySelector('.hero-slider .hero-slider__background');
+  testEl.style.backgroundColor = '#0f0';*/
+  let currentSliderItem = 1;
+  const sliderItems = document.querySelectorAll('.hero-slider__item');
+  const sliderButtonPrev = document.querySelector('.hero-slider__prev');
+  const sliderButtonNext = document.querySelector('.hero-slider__next');
+  let maxItems = sliderItems.length;
+});
