d5bb3ea2547d9b9253751fa2b77b00a9991a3949
DvoVlad <dvovlad@mail.ru>
2024-06-14 15:15:26 +0000

    Deploying to gh-pages from @ DvoVlad/grading-drink2go@7404a63378a835fa236dae7e20c9320fc32e0166 🚀

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,1 +1,1 @@
-window.addEventListener("load",()=>{let t=document.querySelector(".burger-button"),e=document.querySelector(".navigation__list");t.addEventListener("click",()=>{e.classList.toggle("navigation__list--active")})});
+window.addEventListener("load",()=>{let t=document.querySelector(".burger-button"),e=document.querySelector(".navigation__list");t.addEventListener("click",()=>{e.classList.toggle("navigation__list--active"),t.classList.toggle("burger-button--cross")})});
